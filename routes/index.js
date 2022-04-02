3348992bd38ba816469aea173503b5f2f8fd8747
adidam23 <aditi.dam@gmail.com>
2022-04-02 13:56:35 -0400

    more stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MBTI Personality Finder', style: "index" });
+  res.render('index', { title: 'MBTI Personality Predictor', style: "index" });
 });
 
 module.exports = router;
